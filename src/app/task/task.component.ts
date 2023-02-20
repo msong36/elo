89fb3b445aaff61f6f4dbb68b0f3c1fa26e8ae69
Andrew Song <msong36@ucsc.edu>
2023-02-20 14:58:24 -0800

    can take inputs, added logic to radio buttons

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-task',
@@ -7,13 +7,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskComponent implements OnInit {
 
+  // I/O
+  @Input() inputArray: Array<[string, string]> = [];
+  img1: string = "";
+  img2: string = "";
+
   constructor() { }
 
   ngOnInit(): void {
+    this.img1 = this.inputArray[this.currentTask][0];
+    this.img2 = this.inputArray[this.currentTask][1];
   }
 
+  // local UI state
+  currentTask = 0;
+
+  // functions
   help() {
     console.log("TODO: help modal");
+  }
+
+  next() {
+    // check if a radio button is checked
+    var radioCheck = document.querySelector('input[name="avatars"]:checked');
+    if (!radioCheck) {
+      console.log("Nothing checked");
+      return;
+    }
+
+    // get selected value
+    let selectedValue = (radioCheck as HTMLInputElement).value;
+    console.log(selectedValue + ' selected');
+
+    // uncheck radio for next
+    var elements = document.getElementsByTagName('input');
+    for (var i = 0; i < elements.length; i++) {
+      if (elements[i].type == 'radio') {
+        elements[i].checked = false;
+      }
+    }
+
+    // modulus only for testing purposes
+    // need to add functionality for when all tasks are completed
+    this.currentTask = (this.currentTask + 1) % this.inputArray.length;
+
+    // set new img url links
+    this.img1 = this.inputArray[this.currentTask][0];
+    this.img2 = this.inputArray[this.currentTask][1];
   }
 
 }
